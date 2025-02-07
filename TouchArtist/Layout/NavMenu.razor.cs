bc99fce11283457d4ecdd5bcb4fad4efe24642db
XSpace-WolfX <corentinbeck@outlook.fr>
2025-02-07 16:49:34 +0200

    Add project files.

---
+++ TouchArtist/Layout/NavMenu.razor.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace TouchArtist.Layout
+{
+    public class NavMenuBase : ComponentBase
+    {
+        private bool collapseNavMenu = true;
+
+        public string? NavMenuCssClass => collapseNavMenu ? "collapse" : null;
+
+        public void ToggleNavMenu()
+        {
+            collapseNavMenu = !collapseNavMenu;
+        }
+    }
+}
